fa210539a0b7c6bd6d54020e4e3ba3849287fdb4
Knut Kiesel <kiesel@cern.ch>
2015-06-24 09:08:56 +0200

    working for lx1b

--- histogramProducer/run.C
+++ histogramProducer/run.C
@@ -2,12 +2,13 @@
 // https://github.com/cms-susy-photon-rwth-1b/TreeWriter
 // The lib should be in the CMSSW lib path
 
-R__LOAD_LIBRARY(TreeParticles)
+//R__LOAD_LIBRARY(TreeParticles)
 
 // start with root -l -q run.C
 // Using ROOT6
-void run( string infile="photon_ntuple_mva_mini_38.root") {
-//void run( string infile="/user/kiesel/nTuples/QCD_HT_1000ToInf_2_nTuple.root") {
+//void run( string infile="photon_ntuple_mva_mini_38.root") {
+void run( string infile="/user/kiesel/nTuples/QCD_HT_1000ToInf_2_nTuple.root") {
+  gSystem->Load("pluginTreeWriterTreeWriterAuto.so");
 
   TChain ch( "TreeWriter/eventTree" );
   ch.AddFile( infile.c_str() );
@@ -15,6 +16,9 @@ void run( string infile="photon_ntuple_mva_mini_38.root") {
   //ch.AddFile( "/user/kiesel/nTuples/GJets_HT-600toInf_nTuple.root" );
   //ch.AddFile( "/user/kiesel/nTuples/ZJetsToNuNu_HT-400to600_nTuple.root" );
   //ch.AddFile( "/user/kiesel/nTuples/ZJetsToNuNu_HT-600toInf_nTuple.root" );
+
+  double start_time = time(NULL);
   ch.Process( "HistogramProducer.cc+" );
+  cout << "Job needed " << 1.*( time(NULL) - start_time)/360 << " min real time." << endl;
 
 }
