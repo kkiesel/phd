5c2310a7b4192bff3e98ae49dbe11fffcaa62f3a
Knut Kiesel <kiesel@cern.ch>
2015-10-30 14:33:08 +0100

    outsourcing functions and simpler ratio calculation using 'TH1::Divide'

--- plotter/ratio.py
+++ plotter/ratio.py
@@ -4,47 +4,57 @@ from math import sqrt
 import auxiliary as aux
 
 
+def clearXaxisCurrentPad():
+    # Delete label and title of all histograms in the current pad
+    for ding in ROOT.gPad.GetListOfPrimitives():
+        if isinstance( ding, ROOT.TH1 ) or isinstance( ding, ROOT.THStack ):
+            xaxis = ding.GetXaxis()
+            xaxis.SetLabelSize(0)
+            xaxis.SetLabelColor(0)
+            xaxis.SetLabelOffset(1000)
+            xaxis.SetTitle("")
+            xaxis.SetTitleColor(0)
+            xaxis.SetTitleSize(0)
+
+def createBottomPad( r=.2 ):
+    # r: the ratio in which the pad is splitted
+    ROOT.gPad.SetBottomMargin( r + (1-r)*ROOT.gPad.GetBottomMargin() - r*ROOT.gPad.GetTopMargin() )
+    rPad = ROOT.TPad( "rPad", "ratio", 0, 0, 1, 1 )
+    rPad.SetTopMargin( (1-r) - (1-r)*rPad.GetBottomMargin() + r*rPad.GetTopMargin() )
+    rPad.SetFillStyle(3955)
+    rPad.Draw()
+    rPad.cd()
+    rPad.SetLogy(0)
+
+def convertToTH1( profile ):
+    h = profile.ProjectionX( aux.randomName() )
+    h.SetLineColor( profile.GetLineColor() )
+    return h
+
 class Ratio:
     def __init__( self, title, numerator, denominator, sysHisto=None ):
-        if isinstance( numerator, ROOT.TProfile ):
-            numerator_cl = numerator.ProjectionX( aux.randomName() )
-            numerator_cl.SetLineColor( numerator.GetLineColor() )
-            numerator = numerator_cl
-        if isinstance( denominator, ROOT.TProfile ):
-            denominator_cl = denominator.ProjectionX( aux.randomName() )
-            denominator_cl.SetLineColor( denominator.GetLineColor() )
-            denominator = denominator_cl
+
+        # convcert TProfiles to histograms
+        if isinstance( numerator, ROOT.TProfile ): numerator = convertToTH1( numerator )
+        if isinstance( denominator, ROOT.TProfile ): denominator = convertToTH1( denominator )
 
         self.title = title
         self.numerator = numerator
         self.denominator = denominator
         self.sysHisto = sysHisto
         self.ratio = numerator.Clone( aux.randomName() )
+        self.ratioStat = denominator.Clone( aux.randomName() )
         self.ratioSys = denominator.Clone( aux.randomName() )
         self.totalUncert = denominator.Clone( aux.randomName() )
-        self.allowUnsymmetricYaxis_ = False
+        self.allowUnsymmetricYaxis = False
 
     def calculateRatio( self ):
-        for bin in range(self.numerator.GetNbinsX()+2):
-            if self.denominator.GetBinContent(bin):
-                self.ratio.SetBinContent( bin, self.numerator.GetBinContent(bin) / self.denominator.GetBinContent(bin) )
-                self.ratio.SetBinError(   bin, self.numerator.GetBinError(bin)   / self.denominator.GetBinContent(bin) )
-                if self.sysHisto:
-                    self.ratioSys.SetBinContent( bin, self.sysHisto.GetBinContent(bin) / self.denominator.GetBinContent(bin) )
-                    self.ratioSys.SetBinError(   bin, self.sysHisto.GetBinError(bin)   / self.denominator.GetBinContent(bin) )
-                    combinedError = sqrt( self.sysHisto.GetBinError(bin)**2 + self.denominator.GetBinError(bin)**2 )
-                else:
-                    self.ratioSys.SetBinContent( bin, 1 )
-                    self.ratioSys.SetBinError( bin, 0 )
-                    combinedError = self.denominator.GetBinError(bin)
-                self.totalUncert.SetBinError( bin, combinedError/self.denominator.GetBinContent(bin) )
-                self.totalUncert.SetBinContent( bin, 1 )
-            elif self.numerator.GetBinContent(bin):
-                self.ratio.SetBinError( bin, 1.*self.numerator.GetBinError(bin)/self.numerator.GetBinContent(bin) )
-                self.ratio.SetBinContent(bin,0)
+        self.ratio.Divide( self.denominator )
+        self.ratioStat.Divide( self.denominator )
+        if self.sysHisto:
+            self.ratioSys.Divide( self.denominator )
+            self.totalUncert.Add( self.ratioSys, self.ratioStat, .5, .5 )
 
-    def allowUnsymmetricYaxis( self, allow=True ):
-        self.allowUnsymmetricYaxis_ = allow
 
     def getYrange( self ):
         # If no minimum or maximum is specified, choose a minimum from 0 to .5
@@ -64,7 +74,7 @@ class Ratio:
 
         yValues = [ self.ratio.GetBinContent(bin) for bin in range( self.ratio.GetNbinsX()+2 ) ]
         yValues = filter( lambda a: a != 0, yValues )
-        if self.allowUnsymmetricYaxis_:
+        if self.allowUnsymmetricYaxis:
             return yMin, yMax
         else:
             yValuesAbsDiff = [ abs(x-1) for x in yValues ]
@@ -79,15 +89,14 @@ class Ratio:
             return 1-y, 1+y
 
 
-    def draw( self ):
+    def draw( self, yMin=.9, yMax=1.1 ):
         self.calculateRatio()
 
         #yMin, yMax = self.getYrange()
-        yMin, yMax = [0.8, 1.2]
 
         # Set ratio properties
-        for hist in [ self.ratio, self.ratioSys, self.totalUncert ]:
-            hist.GetYaxis().SetNdivisions(2, 0, 2)
+        for hist in [ self.ratio, self.ratioSys, self.ratioStat, self.totalUncert ]:
+            hist.GetYaxis().SetNdivisions(2, 5, 2)
             hist.SetTitleOffset(1.2, "Y")
             hist.SetYTitle( self.title )
             hist.SetMinimum( yMin )
@@ -103,37 +112,22 @@ class Ratio:
         self.totalUncert.SetFillColor( self.denominator.GetLineColor() )
         self.totalUncert.SetLineColor(0)
 
+        self.ratioStat.SetLineWidth(5)
+        self.ratioStat.SetLineColor(ROOT.kGray)
 
-        # Delete label and title of all histograms in the current pad
-        for ding in ROOT.gPad.GetListOfPrimitives():
-            if isinstance( ding, ROOT.TH1 ) or isinstance( ding, ROOT.THStack ):
-                xaxis = ding.GetXaxis()
-                xaxis.SetLabelSize(0)
-                xaxis.SetLabelColor(0)
-                xaxis.SetLabelOffset(1000)
-                xaxis.SetTitle("")
-                xaxis.SetTitleColor(0)
-                xaxis.SetTitleSize(0)
-
-
-        csf = 0.2 # the ratio in which the pad is splitted
-        ROOT.gPad.SetBottomMargin( csf + (1-csf)*ROOT.gPad.GetBottomMargin() - csf*ROOT.gPad.GetTopMargin() )
-        rPad = ROOT.TPad( "rPad", "ratio", 0, 0, 1, 1 )
-        rPad.SetTopMargin( (1-csf) - (1-csf)*rPad.GetBottomMargin() + csf*rPad.GetTopMargin() )
-        rPad.SetFillStyle(3955)
-        rPad.Draw()
-        rPad.cd()
-        rPad.SetLogy(0)
+        clearXaxisCurrentPad()
+        createBottomPad()
 
         self.totalUncert.Draw("e2")
+        self.ratioStat.Draw("same e")
         if self.sysHisto:
             self.ratioSys.Draw("e2 same")
         self.ratio.Draw("e0 same")
+
         if yMin < 1 and yMax > 1:
             oneLine = ROOT.TLine()
             oneLine.SetLineStyle(2)
             axis = self.ratio.GetXaxis()
-            oneLine.DrawLine( self.ratio.GetBinLowEdge( axis.GetFirst() ), 1.0,
-                self.ratio.GetBinLowEdge( axis.GetLast() ), 1.0 )
+            oneLine.DrawLine( axis.GetXmin(), 1.0, axis.GetXmax(), 1.0 )
 
 
