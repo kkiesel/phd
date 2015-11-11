eba42e3d3d6aa5d534c80fe4f0c3071eee4db386
Knut Kiesel <kiesel@cern.ch>
2015-11-11 15:23:06 +0100

    adding photon cut flow tool,

---
+++ histogramProducer/CutFlow.hpp
@@ -0,0 +1,114 @@
+#include "TreeParticles.hpp"
+
+class CutFlowPhoton {
+  public:
+
+    CutFlowPhoton(
+      float sigmaIetaIeta_eb, float cIso_eb, float nIso1_eb, float nIso2_eb, float nIso3_eb, float pIso1_eb, float pIso2_eb,
+      float sigmaIetaIeta_ee, float cIso_ee, float nIso1_ee, float nIso2_ee, float nIso3_ee, float pIso1_ee, float pIso2_ee
+    ){
+      cut_sigmaIetaIeta_eb = sigmaIetaIeta_eb;
+      cut_cIso_eb = cIso_eb;
+      cut_nIso1_eb = nIso1_eb;
+      cut_nIso2_eb = nIso2_eb;
+      cut_nIso3_eb = nIso3_eb;
+      cut_pIso1_eb = pIso1_eb;
+      cut_pIso2_eb = pIso2_eb;
+
+      cut_sigmaIetaIeta_ee = sigmaIetaIeta_ee;
+      cut_cIso_ee = cIso_ee;
+      cut_nIso1_ee = nIso1_ee;
+      cut_nIso2_ee = nIso2_ee;
+      cut_nIso3_ee = nIso3_ee;
+      cut_pIso1_ee = pIso1_ee;
+      cut_pIso2_ee = pIso2_ee;
+    };
+
+    bool pass(){
+      return pass_pt
+        && pass_eta
+        && pass_hoe
+        && pass_sie
+        && pass_cIso
+        && pass_nIso
+        && pass_pIso;
+    }
+
+
+    bool check( const tree::Photon& photon ){
+
+      float pt = photon.p.Pt();
+      float aEta = fabs( photon.p.Eta() );
+
+      // kinematic criteria
+      pass_pt = pt > 15;
+      pass_EB = aEta < 1.4442;
+      pass_EE = 1.566 < aEta && aEta < 2.5;
+      pass_eta = pass_EB || pass_EE;
+
+      // identification & isolation
+      pass_hoe = photon.hOverE < 0.05;
+      pass_sie = false;
+      pass_cIso = false;
+      pass_nIso = false;
+      pass_pIso = false;
+
+      if( pass_EB ) {
+        pass_sie = photon.sigmaIetaIeta < cut_sigmaIetaIeta_eb;
+        pass_cIso = photon.isoChargedHadronsEA < cut_cIso_eb;
+        pass_nIso = photon.isoNeutralHadronsEA < cut_nIso1_eb + exp( cut_nIso2_eb * pt+ cut_nIso3_eb );
+        pass_pIso = photon.isoPhotonsEA < cut_pIso1_eb + cut_pIso2_eb * pt;
+      } else if( pass_EE ) {
+        pass_sie = photon.sigmaIetaIeta < cut_sigmaIetaIeta_ee;
+        pass_cIso = photon.isoChargedHadronsEA < cut_cIso_ee;
+        pass_nIso = photon.isoNeutralHadronsEA < cut_nIso1_ee + exp( cut_nIso2_ee * pt+ cut_nIso3_ee );
+        pass_pIso = photon.isoPhotonsEA < cut_pIso1_ee + cut_pIso2_ee * pt;
+      }
+
+      return pass();
+    };
+
+    bool passPt(){ return pass_pt; };
+    bool passEta(){ return pass_eta; };
+    bool passEB(){ return pass_EB; };
+    bool passEE(){ return pass_EE; };
+    bool passHoe(){ return pass_hoe; };
+    bool passSie(){ return pass_sie; };
+    bool passCIso(){ return pass_cIso; };
+    bool passNIso(){ return pass_nIso; };
+    bool passPIso(){ return pass_pIso; };
+    bool passIso(){ return pass_nIso && pass_cIso && pass_pIso; };
+
+
+ private:
+    bool pass_pt;
+    bool pass_eta;
+    bool pass_EB;
+    bool pass_EE;
+    bool pass_hoe;
+    bool pass_sie;
+    bool pass_cIso;
+    bool pass_nIso;
+    bool pass_pIso;
+
+    float cut_sigmaIetaIeta_eb;
+    float cut_cIso_eb;
+    float cut_nIso1_eb;
+    float cut_nIso2_eb;
+    float cut_nIso3_eb;
+    float cut_pIso1_eb;
+    float cut_pIso2_eb;
+
+    float cut_sigmaIetaIeta_ee;
+    float cut_cIso_ee;
+    float cut_nIso1_ee;
+    float cut_nIso2_ee;
+    float cut_nIso3_ee;
+    float cut_pIso1_ee;
+    float cut_pIso2_ee;
+
+
+};
+
+//loose = CutFlowPhoton( 0.0103, 2.44, 2.57, 0.0044, 0.5809, 1.92, 0.0043, 0.0277, 1.84, 4.00, 0.0040, 0.9402, 2.15, 0.0041 );
+//tight = CutFlowPhoton( 0.0100, 0.91, 0.33, 0.0044, 0.5809, 0.61, 0.0043, 0.0267, 0.65, 0.93, 0.0040, 0.9402, 0.54, 0.0041 );
