821a2016fb4e5c10a2f34923f6290bb3e2a12259
Knut Kiesel <kiesel@cern.ch>
2016-03-09 06:53:13 +0100

    roofit working now

--- plotter/fakeRate.py
+++ plotter/fakeRate.py
@@ -59,74 +59,104 @@ Gaussian while modeling the background with an error function times an exponenti
 we fit the signal with a Crystal Ball convolved with a Breit-Wigner and then fit the background
 with an exponential function only.
 """
-def makeFit( hist ):
-    ROOT.gPad.SetLogy(0)
+def makeFit( hist, name=None ):
+
+    #return aux.integralAndError(hist, 80, 100, False)
+    ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.WARNING)
 
     # variable
-    x = ROOT.RooRealVar( "x", "x", 00., 220. )
+    x = ROOT.RooRealVar( "x", "x", 80., 100. )
     x.setBins(10000,"cache")
     x.setMin("cache",70.5)
-    x.setMax("cache",150.5)
+    x.setMax("cache",110.5)
 
     # breit wiegner
-    m0 = ROOT.RooRealVar( "m0", "m0", 90.5 )
-    width = ROOT.RooRealVar( "width", "width", 2.49 )
+    m0 = ROOT.RooRealVar( "m0", "m0", 91.1876 )
+    width = ROOT.RooRealVar( "width", "width", 2.4952 )
     bw = ROOT.RooBreitWigner( "bw", "bw", x, m0, width )
 
     # crystal ball
-    mean = ROOT.RooRealVar( "mean", "mean", 0. )
-    sigma = ROOT.RooRealVar( "sigma", "sigma", 2.6 )
-    alpha = ROOT.RooRealVar( "alpha", "alpha", 1.2 )
-    n = ROOT.RooRealVar( "n", "n", 0.81 )
+    mean = ROOT.RooRealVar( "mean", "mean", 0., -1, 1 )
+    sigma = ROOT.RooRealVar( "sigma", "sigma", 1.6, 0, 2 )
+    alpha = ROOT.RooRealVar( "alpha", "alpha", 1.2, 0, 10 )
+    n = ROOT.RooRealVar( "n", "n", 0.81, 0, 5 )
     cb = ROOT.RooCBShape( "cb", "cb", x, mean, sigma, alpha, n )
 
+
     # convolute breit wiegner with crystal ball
-    pdf = ROOT.RooFFTConvPdf( "pdf", "pdf", x, bw, cb )
+    sig = ROOT.RooFFTConvPdf( "sig", "sig", x, bw, cb )
+    #sig = ROOT.RooVoigtian( "voigtian","vog", x, mean, width, sigma )
 
-    # define exponentional
-    gamma = ROOT.RooRealVar( "gamma", "gamma", -.9 )
-    exp = ROOT.RooExponential( "exp", "exp", x, gamma )
-
-    # crystal ball
-    mean_bk = ROOT.RooRealVar( "mean_bk", "mean_bk", 80 )
-    sigma_bk = ROOT.RooRealVar( "sigma_bk", "sigma_bk", 200 )
-    alpha_bk = ROOT.RooRealVar( "alpha_bk", "alpha_bk", 1.2 )
-    n_bk = ROOT.RooRealVar( "n_bk", "n_bk", 0.81 )
-    cb_bk = ROOT.RooCBShape( "cb_bk", "cb_bk", x, mean_bk, sigma_bk, alpha_bk, n_bk )
-
+    # define background function
+    a0 = ROOT.RooRealVar("a0","a0",10, 0, 1000)
+    a1 = ROOT.RooRealVar("a1","a1",0,-2,0)
+    a2 = ROOT.RooRealVar("a2","a2",1)
+    bkg = ROOT.RooPolynomial("p2","p2",x,ROOT.RooArgList(a0,a1),0)
 
 
     # add signal + background
-    sig1frac = ROOT.RooRealVar("sig1frac","fraction of component 1 in signal",0.8)
-    #model = ROOT.RooAddPdf("model","sig+bkg",ROOT.RooArgList(exp,pdf), sig1frac)
-    model = ROOT.RooAddPdf("model","sig+bkg",pdf, cb_bk, sig1frac)
+    nSig = ROOT.RooRealVar("nSig","nSig",0,1e5)
+    nBkg = ROOT.RooRealVar("nBkg","nBkg",0,1e5)
 
+    sig_ext = ROOT.RooExtendPdf("esig","extended signal p.d.f",sig,nSig,"signalRange")
+    bkg_ext = ROOT.RooExtendPdf("ebkg","extended background p.d.f",bkg,nBkg,"signalRange")
+
+    model = ROOT.RooAddPdf("model","sig+bkg",ROOT.RooArgList(sig_ext, bkg_ext))
 
     # import data
     dh = ROOT.RooDataHist("dh","dh",ROOT.RooArgList(x),ROOT.RooFit.Import(hist))
 
     # do the fit
-    r = pdf.fitTo(dh, ROOT.RooFit.Save())
-    r.Print()
+    r = model.fitTo(dh, ROOT.RooFit.Save())
+    #r.Print()
 
     plot = x.frame()
     plot.SetTitle(";m (GeV)")
     dh.plotOn( plot )
     model.plotOn( plot )
     plot.Draw()
-    aux.save( "fakeRate_test" )
+    ROOT.gPad.SetLogy(0)
+    if name: aux.save( "fakeRate_rooFit_{}".format(name) )
+    return nSig.getVal(), nSig.getPropagatedError(r)
 
 
 
-
-def main():
-    inFileName = "../histogramProducer/DYJetsToLL_M-50_fakeRate.root"
-    effTot = aux.getFromFile( inFileName, "eta" )
+def fakeRate1d( inFileName, hname ):
+    effTot = aux.getFromFile( inFileName, hname )
     h2P = effTot.GetPassedHistogram()
     h2T = effTot.GetTotalHistogram()
 
-    hP = h2P.ProjectionX()
-    hT = h2T.ProjectionX()
+    hfakeP = h2P.ProjectionY("py1")
+    hfakeT = h2T.ProjectionY("py2")
+    hfakeP.Reset()
+    hfakeT.Reset()
+
+    for yBin in range(h2T.GetNbinsY()+2):
+        hP = h2P.ProjectionX("px1", yBin, yBin)
+        if hP.Integral()<1: continue
+        cP, eP = makeFit( hP, "p_bin{}".format(yBin) )
+        hfakeP.SetBinContent(yBin,cP)
+        hfakeP.SetBinError(yBin,eP)
+
+        hT = h2T.ProjectionX("px1", yBin, yBin)
+        if hT.Integral()<1: continue
+        cT, eT = makeFit( hT, "t_bin{}".format(yBin) )
+        hfakeT.SetBinContent(yBin,cT)
+        hfakeT.SetBinError(yBin,eT)
+
+    c = ROOT.TCanvas()
+    eff = ROOT.TGraphAsymmErrors()
+    eff.BayesDivide( hfakeP, hfakeT )
+    eff.Draw()
+    aux.save("fakeRate_{}".format(hname))
+
+def plotBoth( inFileName, hname ):
+    effTot = aux.getFromFile( inFileName, hname )
+    h2P = effTot.GetPassedHistogram()
+    h2T = effTot.GetTotalHistogram()
+
+    hP = h2P.ProjectionX("px1")
+    hT = h2T.ProjectionX("px2")
 
     hP.SetLineColor(ROOT.kRed)
 
@@ -137,8 +167,15 @@ def main():
     ROOT.gPad.SetLogy(1)
     aux.save("fakeRate_both")
 
-    makeFit( hP )
+    c1, e1 = makeFit( hP, "test" )
 
+
+
+
+def main():
+    inFileName = "../histogramProducer/DYJetsToLL_M-50_fakeRate.root"
+    #fakeRate1d( inFileName, "pt" )
+    plotBoth( inFileName, "eta" )
 
 
 if __name__ == "__main__":
