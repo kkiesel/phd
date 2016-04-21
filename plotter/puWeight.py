1a15205fd141b85f494e4f9e095998fe34c4c7e3
Knut Kiesel <kiesel@cern.ch>
2016-04-21 07:37:34 +0200

    adding 76X and 74X simulation

--- plotter/puWeight.py
+++ plotter/puWeight.py
@@ -17,12 +17,13 @@ if __name__ == "__main__":
 
     h_data = aux.getFromFile( path+"nTrueVertexData.root", "pileup" )
     h_mc = aux.getFromFile( path+"nTrueVertexMC.root", "mix_2015_25ns_Startup_PoissonOOTPU" )
+    h_mc2 = aux.getFromFile( path+"nTrueVertexMC.root", "mix_2015_25ns_FallMC_matchData_PoissonOOTPU" )
     h_weight = aux.getFromFile( path+"data/puWeights.root", "pileupWeight_mix_2015_25ns_Startup_PoissonOOTPU" )
+    h_weight2 = aux.getFromFile( path+"data/puWeights.root", "pileupWeight_mix_2015_25ns_FallMC_matchData_PoissonOOTPU" )
 
-    for h in h_data, h_mc,h_weight:
+    for h in h_data, h_mc, h_mc2, h_weight, h_weight2:
         h.SetTitle(";number of pileup events;Events")
         h.GetXaxis().SetRangeUser(0, 30 )
-
 
     h_data.drawOption_ = "ep"
     h_data.SetLineColor( ROOT.kBlack )
@@ -33,14 +34,17 @@ if __name__ == "__main__":
     h_mc.Scale( h_data.Integral() )
     h_mc.SetLineColor( ROOT.kBlue )
 
+    h_mc2.Scale( h_data.Integral() )
+    h_mc2.SetLineColor( ROOT.kRed )
 
     can = ROOT.TCanvas()
     m = multiplot.Multiplot()
-    m.leg.SetY1(.8)
-    m.leg.SetX1(.7)
+    m.leg.SetY1(.7)
+    m.leg.SetX1(.6)
     m.leg.SetX2(.99)
     m.add( h_data, "Data" )
-    m.add( h_mc, "Simulation" )
+    m.add( h_mc2, "76X (SM)" )
+    m.add( h_mc, "74X (Signal)" )
 
     m.Draw()
 
@@ -50,7 +54,10 @@ if __name__ == "__main__":
 
     h_weight.SetMaximum(3)
     h_weight.SetTitle( ";number of pileup events;Data/Sim." )
+    h_weight.SetLineColor(h_mc.GetLineColor())
     h_weight.Draw("hist")
+    h_weight2.SetLineColor(h_mc2.GetLineColor())
+    h_weight2.Draw("hist same")
 
 
     l = aux.Label()
