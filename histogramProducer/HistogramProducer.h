0ed1b05d8f54512a48f49c993a36254eac9a13ea
Knut Kiesel <kiesel@cern.ch>
2016-09-07 15:03:18 +0200

    moving header to header file

---
+++ histogramProducer/HistogramProducer.h
@@ -0,0 +1,97 @@
+#include "regex"
+#include "time.h"
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TSelector.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TEfficiency.h"
+#include "TRandom2.h"
+#include "TTree.h"
+
+#include "TreeParticles.hpp"
+#include "UserFunctions.h"
+#include "Weighter.h"
+#include "CutFlow.h"
+
+
+class HistogramProducer : public TSelector {
+ public:
+
+  HistogramProducer();
+  virtual ~HistogramProducer() { }
+
+  virtual void Init(TTree *tree);
+  virtual void SlaveBegin(TTree *tree);
+  virtual Bool_t Process(Long64_t entry);
+  virtual void Terminate();
+  virtual Int_t Version() const { return 2; }
+
+  void resetSelection();
+  void defaultSelection();
+  void fillSelection(string const& s);
+  void fillSelectedPhotons(const tree::Particle& p);
+  tree::Jet* matchedJet(const tree::Particle& p);
+
+  void initTriggerStudies();
+  void fillTriggerStudies();
+
+  void initUncut();
+  void fillUncut();
+
+  int genMatch(const tree::Particle& p);
+
+  TTreeReader fReader;
+  TTreeReaderValue<std::vector<tree::Photon>> photons;
+  TTreeReaderValue<std::vector<tree::Jet>> jets;
+  TTreeReaderValue<std::vector<tree::Electron>> electrons;
+  TTreeReaderValue<std::vector<tree::Muon>> muons;
+  TTreeReaderValue<std::vector<tree::Particle>> genJets;
+  TTreeReaderValue<std::vector<tree::GenParticle>> genParticles;
+  TTreeReaderValue<std::vector<tree::IntermediateGenParticle>> intermediateGenParticles;
+  TTreeReaderValue<tree::MET> met;
+  TTreeReaderValue<tree::MET> metRaw;
+  TTreeReaderValue<Float_t> pu_weight;
+  TTreeReaderValue<Char_t> mc_weight;
+  TTreeReaderValue<Int_t> nGoodVertices;
+  TTreeReaderValue<Float_t> genHt;
+  TTreeReaderValue<ULong64_t> eventNo;
+  TTreeReaderValue<UInt_t> runNo;
+  TTreeReaderValue<UInt_t> lumNo;
+  TTreeReaderValue<Bool_t> hlt_photon90_ht600;
+  TTreeReaderValue<Bool_t> hlt_photon90;
+  TTreeReaderValue<Bool_t> hlt_ht600;
+  TTreeReaderValue<Bool_t> hlt_ht800;
+  TTreeReaderValue<Int_t> hlt_ht600_pre;
+
+  vector<tree::Photon*> selPhotons;
+  vector<tree::Jet*> selJets;
+  vector<tree::Jet*> selBJets;
+  vector<tree::Jet*> selHEJets;
+  vector<tree::Electron*> selElectrons;
+  vector<tree::Muon*> selMuons;
+
+  vector<tree::Photon> artificialPhotons;
+
+  float selW = 1.; // weight
+  float sampleW = 1.;
+
+  map<string,map<string,TH1F>> h1Maps;
+  map<string,map<string,TH2F>> h2Maps;
+  map<string,map<string,TH3F>> h3Maps;
+  map<string,TEfficiency> effMap;
+  map<string,TTree*> treeMap;
+  map<int,pair<int,int>> rawEff_vs_run;
+
+  bool isData;
+
+  CutFlowPhoton looseCutFlowPhoton;
+
+  double startTime;
+  TRandom2 rand;
+  ClassDef(HistogramProducer, 1)
+};
+
