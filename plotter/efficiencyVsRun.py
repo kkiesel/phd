ebace01ccceb5839b33d509af54e1263e475652e
Knut Kiesel <kiesel@cern.ch>
2016-09-08 12:37:55 +0200

    global import

--- plotter/efficiencyVsRun.py
+++ plotter/efficiencyVsRun.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import sys
-if sys.version_info[:2] == (2,6):
-    print "Initialize correct python version first!"
-    sys.exit()
-
-import ROOT
-
-import style
-import auxiliary as aux
-from datasets import *
-
 def getRootMap( filename, dictname="triggerStudies/rawEff_vs_run" ):
     inFile = ROOT.TFile.Open( filename )
     ROOT.gInterpreter.GenerateDictionary("map<int,pair<int,int>>","map")
@@ -29,10 +18,9 @@ def getMergedDict( filenames ):
     for f in filenames[1:]: m.update( rootMap2pyDict( getRootMap( f ) ) )
     return m
 
+def drawEfficiencyVsRun(dataset, saveName=""):
+    map = getMergedDict(dataset.files)
 
-def draw(map, saveName=""):
-
-    import collections
     od = collections.OrderedDict(sorted(map.items()))
 
     hrat = ROOT.TH1F("rat", "#gamma90,HT500 trigger efficiency measured with #gamma90 baseline trigger for H_{T}>600;;#varepsilon", len(map), 0, len(map) )
@@ -60,8 +48,7 @@ def draw(map, saveName=""):
 
 
 def main():
-    dic = getMergedDict( dataHt.files )
-    draw(dic)
+    drawEfficiencyVsRun(dataHt)
 
 
 if __name__ == "__main__":
