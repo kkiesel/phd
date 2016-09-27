6ce1c38cdb9a965cd71fb755986d5c8586249a9b
Knut Kiesel <kiesel@cern.ch>
2016-09-27 17:49:56 +0200

    deleting event mixing, and tree

--- fakeRate/FakeRateSelector.cc
+++ fakeRate/FakeRateSelector.cc
@@ -25,59 +25,6 @@ float m(const TVector3& p1, const TVector3& p2) {
   return (a+b).M();
 }
 
-class FourHists {
-  // Creates four histograms for {signal,background}x{pixelseed, }
- public:
-  FourHists() {}
-  FourHists(const string& name, unsigned nBins, float min, float max, unsigned nMaxMixings_=10) {
-    nMaxMixings = nMaxMixings_;
-    eg_sig = TH2F( ("eg_sig_"+name).c_str(), "", 120, 60, 120, nBins, min, max);
-    ee_sig = TH2F( ("ee_sig_"+name).c_str(), "", 120, 60, 120, nBins, min, max);
-    eg_bkg = TH2F( ("eg_bkg_"+name).c_str(), "", 120, 60, 120, nBins, min, max);
-    ee_bkg = TH2F( ("ee_bkg_"+name).c_str(), "", 120, 60, 120, nBins, min, max);
-    binnedTags = std::vector<std::vector<TVector3>>(nBins+2, std::vector<TVector3>(0));
-    binnedProbes = std::vector<std::vector<TVector3>>(nBins+2, std::vector<TVector3>(0));
-  }
-  ~FourHists() { }
-  void Fill(const TVector3& tag, const TVector3& probe, float val, bool signal) {
-    auto bin = eg_sig.GetYaxis()->FindFixBin(val);
-    auto mll = m(tag, probe);
-    ee_sig.Fill(mll, val);
-    if (signal) eg_sig.Fill(mll, val);
-    for (const auto& t : binnedTags.at(bin)) {
-      if (t.DeltaR(probe)<tpSeperationDRcut) continue;
-      mll = m(t, probe);
-      ee_bkg.Fill(mll, val);
-      if (signal) eg_bkg.Fill(mll, val);
-    }
-    for (const auto& p : binnedProbes.at(bin)) {
-      if (tag.DeltaR(p)<tpSeperationDRcut) continue;
-      mll = m(tag, p);
-      ee_bkg.Fill(mll, val);
-      if (signal) eg_bkg.Fill(mll, val);
-    }
-    if (binnedTags.at(bin).size()<nMaxMixings) {
-      binnedTags.at(bin).push_back(tag);
-      binnedProbes.at(bin).push_back(probe);
-    }
-  }
-  void Write() {
-    eg_sig.Write();
-    ee_sig.Write();
-    eg_bkg.Write();
-    ee_bkg.Write();
-  }
- private:
-  TH2F eg_sig;
-  TH2F eg_bkg;
-  TH2F ee_sig;
-  TH2F ee_bkg;
-  vector<vector<TVector3>> binnedTags;
-  vector<vector<TVector3>> binnedProbes;
-  unsigned nMaxMixings;
-};
-
-
 static const float zBosonMass = 91.1876;
 
 class FakeRateSelector : public TSelector {
@@ -98,29 +45,11 @@ class FakeRateSelector : public TSelector {
   TTreeReaderValue<std::vector<tree::Electron>> electrons;
   TTreeReaderValue<std::vector<tree::Particle>> triggerObjects;
   TTreeReaderValue<tree::MET> met;
-  TTreeReaderValue<tree::MET> metRaw;
-  TTreeReaderValue<Float_t> pu_weight;
-  TTreeReaderValue<Char_t> mc_weight;
   TTreeReaderValue<Int_t> nGoodVertices;
-  TTreeReaderValue<ULong64_t> eventNo;
-  TTreeReaderValue<UInt_t> runNo;
-  TTreeReaderValue<UInt_t> lumNo;
   TTreeReaderValue<Bool_t> hlt;
 
   map<string,TEfficiency> effs;
 
-  FourHists pt;
-  FourHists eta;
-  FourHists vtx;
-
-  TTree outTree;
-  TVector3 tag;
-  TVector3 probe;
-  UInt_t nJet, nVertex;
-  float thisMet, emht, mll;
-  Bool_t hasPixelSeed;
-
-  double startTime;
 };
 
 
@@ -130,13 +59,8 @@ FakeRateSelector::FakeRateSelector():
   electrons(fReader, "electrons"),
   triggerObjects(fReader, "triggerObjects"),
   met(fReader, "met"),
-  metRaw(fReader, "met_raw"),
   nGoodVertices(fReader, "nGoodVertices"),
-  pu_weight(fReader, "pu_weight"),
-  runNo(fReader, "runNo"),
-  hlt(fReader, "HLT_Ele27_eta2p1_WPLoose_Gsf_v"),
-  outTree("fakeTree", ""),
-  startTime(time(NULL))
+  hlt(fReader, "HLT_Ele27_eta2p1_WPLoose_Gsf_v")
 {
 }
 
@@ -147,20 +71,12 @@ void FakeRateSelector::Init(TTree *tree)
 
 void FakeRateSelector::SlaveBegin(TTree *tree)
 {
-  outTree.Branch("tag", &tag);
-  outTree.Branch("probe", &probe);
-  outTree.Branch("mll", &mll);
-  outTree.Branch("met", &thisMet);
-  outTree.Branch("emht", &emht);
-  outTree.Branch("nVertex", &nVertex, "nVertex/i");
-  outTree.Branch("nJet", &nJet, "nJet/i");
-  outTree.Branch("hasPixelSeed", &hasPixelSeed, "hasPixelSeed/O");
-
-  pt = FourHists("pt", 12, 0, 120);
-  eta = FourHists("eta", 300, 0, 3);
-  vtx = FourHists("vtx", 36, 0.5, 36.5);
-
   effs["pt"] = TEfficiency("","", 120, 60, 120, 12, 0, 120);
+  effs["eta"] = TEfficiency("","", 120, 60, 120, 300, 0, 3);
+  effs["vtx"] = TEfficiency("","", 120, 60, 120, 36, 0.5, 36.5);
+  effs["emht"] = TEfficiency("","", 120, 60, 120, 30, 0, 300);
+  effs["jets"] = TEfficiency("","", 120, 60, 120, 21, -0.5, 20.5);
+  effs["met"] = TEfficiency("","", 120, 60, 120, 20, 0, 200);
 }
 
 
@@ -227,26 +143,28 @@ Bool_t FakeRateSelector::Process(Long64_t entry)
     selProbes.push_back(b);
   }
 
-  tag = selTags.at(0)->p;
-  probe = selProbes.at(0)->p;
+  TVector3 tag = selTags.at(0)->p;
+  TVector3 probe = selProbes.at(0)->p;
 
-  hasPixelSeed = selProbes.at(0)->hasPixelSeed;
-  nVertex = *nGoodVertices;
-  thisMet = met->p.Pt();
-  mll = m(tag, probe);
-  emht = 0;
-  nJet = 0;
+  bool hasPixelSeed = selProbes.at(0)->hasPixelSeed;
+  auto nVertex = *nGoodVertices;
+  float thisMet = met->p.Pt();
+  float mll = m(tag, probe);
+  float emht = 0;
+  unsigned nJet = 0;
   for (auto& j : *jets ) {
     if (j.p.Pt()>30 && j.p.Eta()<3 && tag.DeltaR(j.p)>0.4 && probe.DeltaR(j.p)>0.4) {
       nJet++;
       emht += j.p.Pt();
     }
   }
-  outTree.Fill();
-  if (abs(probe.Eta())<1.4442) pt.Fill(tag, probe, probe.Pt(), !hasPixelSeed);
-  if (probe.Pt()>40) eta.Fill(tag, probe, abs(probe.Eta()), !hasPixelSeed);
-  vtx.Fill(tag, probe, nVertex, !hasPixelSeed);
+
+  effs.at("vtx").Fill(!hasPixelSeed, mll, nVertex);
   if (abs(probe.Eta())<1.4442) effs.at("pt").Fill(!hasPixelSeed, mll, probe.Pt());
+  if (probe.Pt()>40) effs.at("eta").Fill(!hasPixelSeed, mll, fabs(probe.Eta()));
+  effs.at("emht").Fill(!hasPixelSeed, mll, emht);
+  effs.at("jets").Fill(!hasPixelSeed, mll, nJet);
+  effs.at("met").Fill(!hasPixelSeed, mll, thisMet);
 
   return kTRUE;
 }
@@ -255,12 +173,8 @@ void FakeRateSelector::Terminate()
 {
   auto outputName = getOutputFilename(fReader.GetTree()->GetCurrentFile()->GetName(), "fake");
   TFile file(outputName.c_str(), "RECREATE");
-  outTree.Write("fakeTree", TObject::kWriteDelete);
-  pt.Write();
-  eta.Write();
-  vtx.Write();
   for (auto& it : effs ) it.second.Write(it.first.c_str());
   file.Close();
-  cout << "Created " << outputName << " in " << (time(NULL) - startTime)/60 << " min" << endl;
+  cout << "Created " << outputName << endl;
 }
 
