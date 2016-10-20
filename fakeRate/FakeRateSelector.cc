c6aab6d953df3551ee34a792c4fb320838a2e6cb
Knut Kiesel <kiesel@cern.ch>
2016-10-20 10:42:30 +0200

    adding mc fake rate

--- fakeRate/FakeRateSelector.cc
+++ fakeRate/FakeRateSelector.cc
@@ -40,12 +40,14 @@ class FakeRateSelector : public TSelector {
   virtual Int_t Version() const { return 2; }
 
   void fillSelection(tree::Electron* tag, tree::Photon* probe, const string& name="");
+  void fillGenSelection(const tree::Photon& probe, const string& name="");
   TTreeReader fReader;
   TTreeReaderValue<std::vector<tree::Photon>> photons;
   TTreeReaderValue<std::vector<tree::Jet>> jets;
   TTreeReaderValue<std::vector<tree::Electron>> electrons;
   TTreeReaderValue<std::vector<tree::Muon>> muons;
   TTreeReaderValue<std::vector<tree::Particle>> triggerObjects;
+  TTreeReaderValue<std::vector<tree::IntermediateGenParticle>> intermediateGenParticles;
   TTreeReaderValue<tree::MET> met;
   TTreeReaderValue<Int_t> nGoodVertices;
   TTreeReaderValue<Bool_t> hlt;
@@ -62,6 +64,7 @@ FakeRateSelector::FakeRateSelector():
   electrons(fReader, "electrons"),
   muons(fReader, "muons"),
   triggerObjects(fReader, "triggerObjects"),
+  intermediateGenParticles(fReader, "intermediateGenParticles"),
   met(fReader, "met"),
   nGoodVertices(fReader, "nGoodVertices"),
   hlt(fReader, "HLT_Ele27_eta2p1_WPLoose_Gsf_v")
@@ -77,6 +80,58 @@ void FakeRateSelector::Init(TTree *tree)
 
 void FakeRateSelector::SlaveBegin(TTree *tree)
 {
+}
+
+void FakeRateSelector::fillGenSelection(const tree::Photon& probe, const string& name) {
+
+  if (!effs.count("gen_pt"+name)) {
+    effs["gen_pt"+name] = TEfficiency("",";#it{p}_{T} (GeV);#varepsilon", 40, 0, 200);
+    effs["gen_met"+name] = TEfficiency("",";#it{E}_{T}^{miss} (GeV);#varepsilon", 40, 0, 200);
+    effs["gen_emht"+name] = TEfficiency("",";#it{EMH}_{T} (GeV);#varepsilon", 40, 0, 400);
+    effs["gen_eta"+name] = TEfficiency("",";|#eta|;#varepsilon", 300, 0, 3);
+    effs["gen_jets"+name] = TEfficiency("",";jet multiplicity;#varepsilon", 21, -0.5, 20.5);
+    effs["gen_vtx"+name] = TEfficiency("",";vertex multiplicity;#varepsilon", 36, 0.5, 36.5);
+    effs["gen_sie"+name] = TEfficiency("",";#sigma_{i#etai#eta};#varepsilon", 60, 0, 0.03);
+    effs["gen_sip"+name] = TEfficiency("",";#sigma_{i#phii#phi};#varepsilon", 80, 0, 0.08);
+    effs["gen_hoe"+name] = TEfficiency("",";H/E;#varepsilon", 100, 0, 0.05);
+    effs["gen_r9"+name] = TEfficiency("",";r9;#varepsilon", 60, 0, 1.1);
+    effs["gen_cIso"+name] = TEfficiency("",";I_{#pm} (GeV);#varepsilon", 50, 0, 5);
+    effs["gen_nIso"+name] = TEfficiency("",";I_{0} (GeV);#varepsilon", 50, 0, 5);
+    effs["gen_pIso"+name] = TEfficiency("",";I_{#gamma} (GeV);#varepsilon", 50, 0, 5);
+    effs["gen_cIsoWorst"+name] = TEfficiency("",";worst I_{#pm} (GeV);#varepsilon", 80, 0, 40);
+    effs["gen_pt_vs_vtx"+name] = TEfficiency("", ";#it{p}_{T};vertex multiplicity;#varepsilon", 40, 0, 200, 36, 0.5, 36.5);
+    effs["gen_met_vs_vtx"+name] = TEfficiency("", ";#it{E}_{T}^{miss};vertex multiplicity;#varepsilon", 40, 0, 200, 36, 0.5, 36.5);
+  }
+
+  bool hasPixelSeed = probe.hasPixelSeed;
+  float thisMet = met->p.Pt();
+  auto nVertex = *nGoodVertices;
+  unsigned nJet = 0;
+  float emht = probe.p.Pt();;
+  for (auto& j : *jets ) {
+    if (j.p.Pt()>30 && j.p.Eta()<3 && probe.p.DeltaR(j.p)>0.4) {
+      nJet++;
+      emht += j.p.Pt();
+    }
+  }
+
+  effs.at("gen_pt"+name).Fill(!hasPixelSeed, probe.p.Pt());
+  effs.at("gen_eta"+name).Fill(!hasPixelSeed, fabs(probe.p.Eta()));
+  effs.at("gen_vtx"+name).Fill(!hasPixelSeed, nVertex);
+  effs.at("gen_emht"+name).Fill(!hasPixelSeed, emht);
+  effs.at("gen_jets"+name).Fill(!hasPixelSeed, nJet);
+  effs.at("gen_met"+name).Fill(!hasPixelSeed, thisMet);
+  effs.at("gen_sie"+name).Fill(!hasPixelSeed, probe.sigmaIetaIeta);
+  effs.at("gen_sip"+name).Fill(!hasPixelSeed, probe.sigmaIphiIphi);
+  effs.at("gen_hoe"+name).Fill(!hasPixelSeed, probe.hOverE);
+  effs.at("gen_r9"+name).Fill(!hasPixelSeed, probe.r9);
+  effs.at("gen_cIso"+name).Fill(!hasPixelSeed, probe.cIso);
+  effs.at("gen_nIso"+name).Fill(!hasPixelSeed, probe.nIso);
+  effs.at("gen_pIso"+name).Fill(!hasPixelSeed, probe.pIso);
+  effs.at("gen_cIsoWorst"+name).Fill(!hasPixelSeed, probe.cIsoWorst);
+
+  effs.at("gen_pt_vs_vtx"+name).Fill(!hasPixelSeed, probe.p.Pt(), nVertex);
+  effs.at("gen_met_vs_vtx"+name).Fill(!hasPixelSeed, thisMet, nVertex);
 }
 
 void FakeRateSelector::fillSelection(tree::Electron* tag, tree::Photon* probe, const string& name) {
@@ -138,6 +193,18 @@ Bool_t FakeRateSelector::Process(Long64_t entry)
   if (!(entry%int(2e6))) cout << 1.*entry / fReader.GetEntries(false) << endl;
   fReader.SetEntry(entry);
   if (!*hlt && isData) return true;
+  if (!isData) { // gen matching fake-rate
+    for (auto& pho : *photons) {
+      if (!pho.isLoose) continue;
+      if (fabs(genMatchWZDecay(pho, *intermediateGenParticles)) != 11) continue;
+      fillGenSelection(pho);
+      if (pho.p.Pt()>40) fillGenSelection(pho, "_40pt");
+      if (abs(pho.p.Eta())<1.4442) fillGenSelection(pho, "_EB");
+      if (abs(pho.p.Eta())<1.4442 && pho.p.Pt()>40) fillGenSelection(pho, "_EB_40pt");
+      if (abs(pho.p.Eta())>0.1 && abs(pho.p.Eta())<1.4442 && pho.p.Pt()>40) fillGenSelection(pho, "_EB_01eta_40pt");
+      if (abs(pho.p.Eta())<1.4442 && pho.p.Pt()>100) fillGenSelection(pho, "_EB_100pt");
+    }
+  }
 
   // search tag
   vector<tree::Electron*> selTags;
