b8a4e5ab8f9e301e94992ee29f66889f950bc5e3
Knut Kiesel <kiesel@cern.ch>
2016-10-20 23:18:17 +0200

    adding shell script

---
+++ histogramProducer/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+export SCRAM_ARCH=slc6_amd64_gcc530
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+
+
+thispath=`pwd`
+cd ~/CMSSW/treewriter/CMSSW_8_0_12/src
+eval `scramv1 runtime -sh`
+cd $thispath
+
+./run.py "$@"
