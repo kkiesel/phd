5484fe4d4c3976a6a75852588237de832415fdcf
Knut Kiesel <kiesel@cern.ch>
2016-10-25 12:41:49 +0200

    adding signal cross section

--- histogramProducer/UserFunctions.h
+++ histogramProducer/UserFunctions.h
@@ -209,6 +209,7 @@ float sampleCrossSection(const string& inputFileName) {
     {"ZGTo2LG", 117.864},
     {"ZGTo2LGmod", 117.864},
     {"DYJetsToLL_M-50", 6025.2},
+    {"SMS-T5Wg_1600_100", 0.00810078},
     {"WGJets_MonoPhoton_PtG-130", 0.834} // source: jlange
   };
   if (xs.count(sampleName)) {
