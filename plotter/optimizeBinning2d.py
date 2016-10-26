745eda41302b8bc3a33b40321865dc26c004c520
Knut Kiesel <kiesel@cern.ch>
2016-10-26 09:03:39 +0200

    also 1d optimization

--- plotter/optimizeBinning2d.py
+++ plotter/optimizeBinning2d.py
@@ -28,10 +28,18 @@ def simplifiedSig(s, b):
     if b<1e-6: b = 2
     return 0.5 * s / math.sqrt(s+b+(0.5*b)**2)
 
-def optimizeBinnigs2d(bkgSet, name, sigPoint):
+def asimovSig(s, b):
+    if not b: b = 1e-10
+    return math.sqrt(2*((s+b)*math.log(1+s/b)-s))
+
+def optimizeBinnigs2d(bkgSet, name, signalSet):
     style.style2d()
-    bkgHist = bkgSet.getHist("tr/metRaw_vs_emht")
-    sigHist = signal[sigPoint].getHist("tr/metRaw_vs_emht")
+    bkgHist = bkgSet.getHist("tr/met_vs_emht")
+    sigHist = signalSet.getHist("tr/met_vs_emht")
+
+    lumiScale=40.00/27.22
+    bkgHist.Scale(lumiScale)
+    sigHist.Scale(lumiScale)
 
     xmax = 800
     ymax = 4000
@@ -43,18 +51,56 @@ def optimizeBinnigs2d(bkgSet, name, sigPoint):
         if xbin > xmaxBin or ybin > ymaxBin: continue
         b = bkgHist.Integral(xbin, -1, ybin, -1)
         s = sigHist.Integral(xbin, -1, ybin, -1)
-        signif = simplifiedSig(s, b)
+        signif = asimovSig(s, b)
         signifHist.SetBinContent(xbin, ybin, signif)
 
     signifHist.GetXaxis().SetRange(0,xmaxBin)
     signifHist.GetYaxis().SetRange(0,ymaxBin)
+    signifHist.SetXTitle("minimum "+signifHist.GetXaxis().GetTitle())
+    signifHist.SetYTitle("minimum "+signifHist.GetYaxis().GetTitle())
+    signifHist.SetZTitle("Asimov significance")
     signifHist.Draw("colz")
-    aux.save("optimizeBinning2d_{}_{}".format(name,sigPoint), log=False)
+    l = aux.Label(info=signalSet.label, drawAll=False)
+    l.lum = ROOT.TLatex( .63, .95, "%.2f fb^{-1} (%s TeV)"%(aux.intLumi/1000.*lumiScale, 13) )
+    l.draw()
+
+    aux.save("optimizeBinning2d_{}_{}".format(name,signalSet.names[0]), log=False)
+
+def optimizeMetBinning(bkgSet, signalSet, name, minHt, maxHt):
+    style.defaultStyle()
+    bkgHist = bkgSet.getHist("tr/met_vs_emht")
+    sigHist = signalSet.getHist("tr/met_vs_emht")
+
+    lumiScale=40.00/27.22
+    bkgHist.Scale(lumiScale)
+    sigHist.Scale(lumiScale)
+
+    minHtBin = bkgHist.GetYaxis().FindFixBin(minHt)
+    maxHtBin = bkgHist.GetYaxis().FindFixBin(maxHt-1e6)
+    bkg1d = bkgHist.ProjectionX(aux.randomName(), minHtBin, maxHt)
+    sig1d = sigHist.ProjectionX(aux.randomName(), minHtBin, maxHt)
+
+    signifHist = sig1d.Clone(aux.randomName())
+    binning = [sig1d.GetNbinsX()+2]
+    while binning[-1] > 1:
+        maxSig = 0
+        maxSigBin = 0
+        for bin in range(binning[-1], -1, -1):
+            b = bkg1d.Integral(bin, binning[-1])
+            s = sig1d.Integral(bin, binning[-1])
+            signif = asimovSig(s, b)
+            if signif > maxSig:
+                maxSig = signif
+                maxSigBin = bin
+        binning.append(maxSigBin)
+    binning = binning[-1:1:-1]
+    print [sig1d.GetBinLowEdge(b) for b in binning]
 
 
 
 if __name__ == "__main__":
-    optimizeBinnigs2d(data, "data", "T5Wg_1550_1500")
-    optimizeBinnigs2d(data, "data", "T5Wg_1550_100")
-    optimizeBinnigs2d(gjets+qcd+ttjets+wjets+znunu+ttg+wg_mg+zg_130, "mc", "T5Wg_1550_1500")
-    optimizeBinnigs2d(gjets+qcd+ttjets+wjets+znunu+ttg+wg_mg+zg_130, "mc", "T5Wg_1550_100")
+    #optimizeBinnigs2d(data, "data", t5wg_1600_100)
+    optimizeMetBinning(data, t5wg_1600_100, "data_highHt", 2000, 5000)
+    optimizeMetBinning(data, t5wg_1600_100, "data_mediumHt", 1200, 2000)
+    optimizeMetBinning(data, t5wg_1600_100, "data_lowHt", 0, 1200)
+    #optimizeBinnigs2d(gjets+qcd+ttjets+wjets+znunu+ttg+wg_mg+zg_130, "mc", "T5Wg_1550_100")
