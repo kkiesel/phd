59bee7b126833343728bb93c75d6f0635497e02c
Knut Kiesel <kiesel@cern.ch>
2016-11-18 10:03:10 +0100

    deleting 2015 mc and adding uncertainty

--- plotter/puWeight.py
+++ plotter/puWeight.py
@@ -1,51 +1,31 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import ROOT
-import os
-
-
-# private libs
-import ratio
-import style
-import multiplot
-
-import auxiliary as aux
+from include import *
 
 if __name__ == "__main__":
     path = os.environ['CMSSW_BASE']+'/src/TreeWriter/PUreweighting/'
 
-    h_data = aux.getFromFile( path+"nTrueVertexData.root", "pileup" )
-    h_mc = aux.getFromFile( path+"nTrueVertexMC.root", "mix_2015_25ns_Startup_PoissonOOTPU" )
-    h_mc2 = aux.getFromFile( path+"nTrueVertexMC.root", "mix_2015_25ns_FallMC_matchData_PoissonOOTPU" )
-    h_weight = aux.getFromFile( path+"data/puWeights.root", "pileupWeight_mix_2015_25ns_Startup_PoissonOOTPU" )
-    h_weight2 = aux.getFromFile( path+"data/puWeights.root", "pileupWeight_mix_2015_25ns_FallMC_matchData_PoissonOOTPU" )
+    h_data = aux.getFromFile(path+"nTrueVertexData.root", "pileup")
+    h_mc = aux.getFromFile(path+"nTrueVertexMC.root", "mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU")
+    h_weight = aux.getFromFile(path+"data/puWeights.root", "pileupWeight_mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU")
+    h_weightUp = aux.getFromFile(path+"data/puWeights.root", "pileupWeightUp_mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU")
+    h_weightDn = aux.getFromFile(path+"data/puWeights.root", "pileupWeightDown_mix_2016_25ns_SpringMC_PUScenarioV1_PoissonOOTPU")
 
-    for h in h_data, h_mc, h_mc2, h_weight, h_weight2:
-        h.SetTitle(";number of pileup events;Events")
-        h.GetXaxis().SetRangeUser(0, 30 )
+    for h in h_data, h_mc, h_weight:
+        h.SetTitle(";;Events")
 
-    h_data.drawOption_ = "ep"
-    h_data.SetLineColor( ROOT.kBlack )
-    h_data.SetMarkerColor( ROOT.kBlack )
-    h_data.SetMarkerStyle( 20 )
-    h_data.SetMarkerSize( 0.5 )
-
-    h_mc.Scale( h_data.Integral() )
-    h_mc.SetLineColor( ROOT.kBlue )
-
-    h_mc2.Scale( h_data.Integral() )
-    h_mc2.SetLineColor( ROOT.kRed )
+    aux.drawOpt(h_data, "data")
+    h_mc.Scale(h_data.Integral())
+    h_mc.SetLineColor(ROOT.kRed)
 
     can = ROOT.TCanvas()
     m = multiplot.Multiplot()
     m.leg.SetY1(.7)
     m.leg.SetX1(.6)
     m.leg.SetX2(.99)
-    m.add( h_data, "Data" )
-    m.add( h_mc2, "76X (SM)" )
-    m.add( h_mc, "74X (Signal)" )
-
+    m.add(h_data, "Data")
+    m.add(h_mc, "Simulation")
     m.Draw()
 
     # just create the pad, the ratio is not used
@@ -53,14 +33,15 @@ if __name__ == "__main__":
     r.draw()
 
     h_weight.SetMaximum(3)
-    h_weight.SetTitle( ";number of pileup events;Data/Sim." )
+    h_weight.SetTitle( ";number of interactions per event;Data/Sim." )
     h_weight.SetLineColor(h_mc.GetLineColor())
     h_weight.Draw("hist")
-    h_weight2.SetLineColor(h_mc2.GetLineColor())
-    h_weight2.Draw("hist same")
+
+    hsys = aux.getSystFromDifference(h_weightUp, h_weightDn)
+    aux.drawOpt(hsys, "sysUnc")
+    hsys.Draw("same e2")
 
 
     l = aux.Label()
-
     can.SaveAs("plots/pu_weight.pdf")
 
