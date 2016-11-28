707fdbc95bb1ae16736495f770b91ed7830b17dc
Knut Kiesel <kiesel@cern.ch>
2016-11-28 07:45:21 +0100

    correcting whitespaces

--- histogramProducer/Weighter.h
+++ histogramProducer/Weighter.h
@@ -1,11 +1,11 @@
 
 class Weighter {
   public:
-    Weighter(){}
-    Weighter( const string& filename, const string& histname ) {
-      TFile f( filename.c_str() );
-      h = (TH1F*)f.Get( histname.c_str() );
-      if(h) {
+    Weighter() {}
+    Weighter(const string& filename, const string& histname) {
+      TFile f(filename.c_str());
+      h = (TH1F*) f.Get(histname.c_str());
+      if (h) {
         h->SetDirectory(0);
       } else {
         cerr << "Error in <Weighter::Weighter>:  Could not find histogram "
@@ -13,14 +13,14 @@ class Weighter {
       }
     }
 
-    ~Weighter(){
-      if(h) delete h;
+    ~Weighter() {
+      if (h) delete h;
     }
 
-    float getWeight( float value ){
-      return h ? h->GetBinContent( h->FindFixBin( value ) ) : 1.;
+    float getWeight(float value) {
+      return h ? h->GetBinContent( h->FindFixBin(value) ) : 1.;
     }
 
   private:
-    TH1F* h=0;
+    TH1F* h = 0;
 };
