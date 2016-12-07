f097824685019470a50a056eb495d361b4f4e895
Knut Kiesel <kiesel@cern.ch>
2016-12-07 16:27:21 +0100

    adding plots

--- plotter/subSelections.py
+++ plotter/subSelections.py
@@ -34,7 +34,7 @@ def subSelection(sampleName, dataset, totalDir, subDirs, name, binning, binningN
         m.addStack(hSub, labels[subDir] if subDir in labels else subDir)
     m.Draw()
 
-    l = aux.Label(sim=dataset!=data, info=dataset.label)
+    l = aux.Label(sim=dataset not in [data,dataHt], info=dataset.label)
 
     if binningName: binningName = "_"+binningName
     aux.save("subSelections_{}_{}{}".format(sampleName, name, binningName), log=False)
@@ -42,24 +42,24 @@ def subSelection(sampleName, dataset, totalDir, subDirs, name, binning, binningN
 
 def subSelections(sampleName, dataset):
     names = aux.getObjectNames(dataset.files[0], "tr", [ROOT.TH1F])
-    names = ["met"]
+    #names = ["met"]
     for name in names:
         for binningName, binning in aux.getBinningsFromName(name).iteritems():
-            if binningName != "3": continue
+            #if binningName != "3": continue
             #subSelection(sampleName+"_electrons", dataset, "tr", ["tr_genE", "tr_noGenE"], name, binning, binningName)
-            subSelection(sampleName+"_electrons", dataset, "tr", ["tr_gen0","tr_gen-11", "tr_gen11", "tr_gen-13", "tr_gen13", "tr_gen-22", "tr_gen22"], name, binning, binningName)
+            subSelection(sampleName+"_electrons", dataset, "tr", ["tr_gen0","tr_gen-11", "tr_gen11", "tr_gen-22", "tr_gen22"], name, binning, binningName)
             subSelection(sampleName+"_electronsWZ", dataset, "tr", ["tr_genWZ0", "tr_genWZ11", "tr_genWZ13", "tr_genWZ15","tr_genWZ12", "tr_genWZ14", "tr_genWZ16"], name, binning, binningName)
             subSelection(sampleName, dataset, "tr", ["tr_unmatched", "tr_true_pi0", "tr_true_other", "tr_true_GUDSCB"], name, binning, binningName)
 
 
 if __name__ == "__main__":
-    subSelections("wjets", wjets)
-    subSelections("tt", ttjets)
-    subSelections("gjets", gjets)
-    subSelections("qcd", qcd)
-    subSelections("ttg", ttg)
-    subSelections("wg", wg)
-    subSelections("znunu", znunu)
-    subSelections("zg", zg)
+    #subSelections("wjets", wjets)
+    #subSelections("tt", ttjets)
+    #subSelections("gjets", gjets)
+    #subSelections("qcd", qcd)
+    #subSelections("ttg", ttg)
+    #subSelections("wg", wg)
+    #subSelections("znunu", znunu)
 
+    subSelection("data", dataHt, "tr_jControl", ["tr_jControl_noLep"], "met", aux.getBinningsFromName("met")["3"], "3")
 
