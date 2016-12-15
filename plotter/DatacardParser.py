55e37d052d4adb6fd6458166d1ade49ad0a87cac
Knut Kiesel <kiesel@cern.ch>
2016-12-15 12:40:58 +0100

    adding return value

--- plotter/DatacardParser.py
+++ plotter/DatacardParser.py
@@ -34,6 +34,7 @@ def addDatacardParserOptions(parser):
     parser.add_option("--X-no-optimize-templates",  dest="optimizeExistingTemplates", default=True, action="store_false", help="Don't optimize templates on the fly (relevant for HZZ)")
     parser.add_option("--X-no-optimize-bound-nusances",  dest="optimizeBoundNuisances", default=True, action="store_false", help="Don't flag nuisances to have a different implementation of bounds")
     parser.add_option("--X-no-optimize-bins",  dest="optimizeTemplateBins", default=True, action="store_false", help="Don't optimize template bins")
+    return parser.parse_args()
 
 
 from Datacard import Datacard
