75c3bdcfe8d86060748e0416f855dd0c183f8294
Knut Kiesel <kiesel@cern.ch>
2017-02-06 23:03:21 +0100

    second electron matching

--- histogramProducer/HistogramProducer.cc
+++ histogramProducer/HistogramProducer.cc
@@ -734,10 +734,14 @@ Bool_t HistogramProducer::Process(Long64_t entry)
     auto dPhi = fabs(met->p.DeltaPhi(g->p));
     bool orthogonal = .3<dPhi;
     bool genE = genMatchWZDecay(*g, *intermediateGenParticles);
+    bool genE2 = fabs(genMatchNegativePrompt(*g, *genParticles)) == 11;
     bool isGenEclean = isData || isSignal || !genE;
 
     if (!cutPrompt && orthogonal && isGenEclean && emht<2000) fillSignalSelection("signal_lowEMHT", addWeight);
     if (!cutPrompt && orthogonal && isGenEclean && 2000<emht) fillSignalSelection("signal_highEMHT", addWeight);
+
+    if (!cutPrompt && orthogonal && genE2 && emht<2000) fillSignalSelection("signal_lowEMHT_genE2", addWeight);
+    if (!cutPrompt && orthogonal && genE2 && 2000<emht) fillSignalSelection("signal_highEMHT_genE2", addWeight);
 
     if (!cutPrompt && orthogonal && genE && emht<2000) fillSignalSelection("signal_lowEMHT_genE", addWeight);
     if (!cutPrompt && orthogonal && genE && 2000<emht) fillSignalSelection("signal_highEMHT_genE", addWeight);
