e87828777983c67e75b9d53ccc925f67bba11969
Knut Kiesel <kiesel@cern.ch>
2017-02-07 11:13:24 +0100

    adding emht cut to label

--- plotter/finalPredictions.py
+++ plotter/finalPredictions.py
@@ -702,7 +702,9 @@ def finalDistributionSignalHist(name, dirSet, dirDir, preSet, preSetElectron, pr
         x = r.draw(0., 2, None)
     else:
         x = r.draw(0., 1.5, m.getStack())
-    l = aux.Label(sim= not dirSet==data, info=dirSet.label if dirSet != data else "")
+    info = "#it{EMH}_{T}<2TeV" if "lowEMHT" in name else "2TeV<#it{EMH}_{T}"
+    if dirSet != data: info += "; "+dirSet.label
+    l = aux.Label(sim= not dirSet==data, info=info)
     aux.save(name, normal=False, changeMinMax=False)
 
 
