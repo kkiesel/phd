c013c445cfdc0f764c8abbbab97d4f33699d4fc0
Knut Kiesel <kiesel@cern.ch>
2017-02-09 13:06:04 +0100

    adding cross sections

--- histogramProducer/UserFunctions.h
+++ histogramProducer/UserFunctions.h
@@ -218,7 +218,9 @@ float sampleCrossSection(const string& inputFileName) {
     {"TGJets_amcatnlo_madspin", 2.967},
     {"DYJetsToLL_M-50", 6025.2},
     {"SMS-T5Wg_1600_100", 0.00810078},
-    {"SMS-T5Wg_1600_1500", 0.00810078}
+    {"SMS-T5Wg_1600_1500", 0.00810078},
+    {"SMS-T5Wg_2000_100", 0.000981077},
+    {"SMS-T6gg_1750_1650", 0.000646271}
   };
   if (xs.count(sampleName)) {
     return xs.at(sampleName);
