8d1776f56dab0ae93d3cb3e5796dedfa266adeb9
Knut Kiesel <kiesel@cern.ch>
2017-02-11 18:42:50 +0100

    proper inclusion of all type of uncertainties

--- plotter/limitTools.py
+++ plotter/limitTools.py
@@ -7,46 +7,6 @@ import optparse
 import os
 from Datacard import Datacard
 
-class Limit:
-    def __init__(self, datacardname):
-        self.datacardname = datacardname
-        self.limitfilename = datacardname+".limit"
-
-        self.obs = -1
-        self.exp = -1
-        self.expUp = -1
-        self.expDn = -1
-        self.exp2Up = -1
-        self.exp2Dn = -1
-        self.rMinNLL = -1
-        self.error = None
-
-    def calculate(self):
-        if not os.path.isfile(self.limitfilename) or os.path.getmtime(self.datacardname)>os.path.getmtime(self.limitfilename):
-            with open(self.limitfilename, "w+") as f:
-                bn = os.path.basename(self.daracardname)
-                out = subprocess.check_output(["combine", "-M", "Asymptotic", self.datacardname, "-n", bn], stderr=subprocess.STDOUT)
-                f.write(out)
-                outputFile = "higgsCombine{}.Asymptotic.mH120.root".format(bn)
-                if os.path.isfile(outputFile): os.remove(outputFile)
-        with open(self.limitfilename) as f:
-            out = f.read()
-
-    def getInfo(self):
-        if not os.path.isfile(self.limitfilename): self.calculate()
-        with open(self.limitfilename) as f:
-            out = f.read()
-        lines = out.split("\n")
-        for line in lines:
-            if line.startswith("NLL at global minimum of asimov:"): self.rMinNLL = float(re.match(".*\(r = (.*)\).*",line).group(1))
-            if line.startswith("Observed Limit: r < "): self.obs    = float(line.split("<")[1])
-            if line.startswith("Expected  2.5%: r < "): self.exp2Dn = float(line.split("<")[1])
-            if line.startswith("Expected 16.0%: r < "): self.expDn  = float(line.split("<")[1])
-            if line.startswith("Expected 50.0%: r < "): self.exp    = float(line.split("<")[1])
-            if line.startswith("Expected 84.0%: r < "): self.expUp  = float(line.split("<")[1])
-            if line.startswith("Expected 97.5%: r < "): self.exp2Up = float(line.split("<")[1])
-            if "ERROR" in line: self.error = line
-
 def infoFromOut(out):
     infos = { "obs":0, "exp":0, "exp1up":0, "exp1dn":0, "exp2up":0, "exp2dn":0 }
     for line in out.split("\n"):
@@ -119,8 +79,25 @@ def getContour( gr2d ):
     return contoursN[-1][0]
 
 class MyDatacard(Datacard):
-    def __init__(self, dc):
-        if isinstance(dc, Datacard):
+    def __init__(self, dc=""):
+        if not dc:
+            self.bins = []
+            self.obs = {}
+            #self.processes = ['signal', 'gqcd', 'ele', 'zg', 'wg', 'ttg']
+            self.processes = []
+            self.signals = ['signal']
+            #self.isSignal = {'wg': False, 'signal': True, 'zg': False, 'gqcd': False, 'ele': False, 'ttg': False}
+            self.isSignal = {}
+            self.keyline = []
+            self.exp = {}
+            #self.systs = [(x, False, 'lnN', [], {}) for x in "lumi", "jec", "pdf", "gqcdSyst", "eleSyst", "wgSyst", "zgSyst", "ttgSyst"]
+            self.systs = []
+            self.shapeMap = {}
+            self.hasShape = False
+            self.flatParamNuisances = {}
+            self.rateParams = {}
+            self.rateParamsOrder = []
+        elif isinstance(dc, Datacard):
             self.bins = dc.bins
             self.obs = dc.obs
             self.processes = dc.processes
@@ -139,20 +116,9 @@ class MyDatacard(Datacard):
             options, b = DatacardParser.addDatacardParserOptions(optparse.OptionParser())
             mydc = MyDatacard(DatacardParser.parseCard(file(dc), options))
             self.__init__(mydc)
-        elif dc == "photonHt":
-            self.bins = []
-            self.obs = {}
-            self.processes = ['signal', 'gqcd', 'ele', 'zg', 'wg', 'ttg']
-            self.signals = ['signal']
-            self.isSignal = {'wg': False, 'signal': True, 'zg': False, 'gqcd': False, 'ele': False, 'ttg': False}
-            self.keyline = []
-            self.exp = {}
-            self.systs = [(x, False, 'lnN', [], {}) for x in "lumi", "jec", "pdf", "gqcdSyst", "eleSyst", "wgSyst", "zgSyst", "ttgSyst"]
-            self.shapeMap = {}
-            self.hasShape = False
-            self.flatParamNuisances = {}
-            self.rateParams = {}
-            self.rateParamsOrder = []
+        else:
+            print "Do not know how to initialize MyDatacard with", dc
+
 
     def __str__(self):
         print "Class MyDatacard"
@@ -163,7 +129,8 @@ class MyDatacard(Datacard):
         print "isSignal           ", self.isSignal
         print "keyline            ", self.keyline
         print "exp                ", self.exp
-        print "systs              ", self.systs
+        for s in self.systs:
+            print "systs              ", s
         #print "shapeMap           ", self.shapeMap
         #print "hasShape           ", self.hasShape
         #print "flatParamNuicances ", self.flatParamNuisances
@@ -202,7 +169,7 @@ class MyDatacard(Datacard):
         table.append(["rate", ""] + [str(self.exp[bN][processNames[i]]) for i, bN in enumerate(binNames)])
         for line in self.systs:
             relUncerts = [line[4][bN][processNames[i]] for i, bN in enumerate(binNames)]
-            table.append([line[0], line[2]] + [str(x) if x!= 1 else "-" for x in relUncerts])
+            table.append([line[0], line[2]] + ["-" if x==1 or x==0 else str(x) for x in relUncerts])
         # format lengts of strings
         columnWidths = [max([len(i) for i in line]) for line in zip(*table)]
         for irow, row in enumerate(table):
@@ -242,6 +209,32 @@ class MyDatacard(Datacard):
         self.systs.append(('ttgStat_'+name, False, 'lnN', [], dict(zip(self.bins, [{'wg': 1, 'signal': 1, 'zg': 1, 'gqcd': 1, 'ele': 1, 'ttg': 1}]*(len(self.bins)-1)+[{'wg': 1, 'signal': 1, 'zg': 1, 'gqcd': 1, 'ele': 1, 'ttg': ttgStat}]))))
         self.systs.append(('signalStat_'+name, False, 'lnN', [], dict(zip(self.bins, [{'wg': 1, 'signal': 1, 'zg': 1, 'gqcd': 1, 'ele': 1, 'ttg': 1}]*(len(self.bins)-1)+[{'wg': 1, 'signal': signalStat, 'zg': 1, 'gqcd': 1, 'ele': 1, 'ttg': 1}]))))
 
+    def addBin(self, name, obs, bkgRates, bkgUncertainties):
+        if self.processes:
+            if self.processes != bkgRates.keys(): print "ERROR: Old processes", self.processes, " New processes:", bkgRates.keys()
+        else:
+            self.processes = bkgRates.keys()
+            self.isSignal = dict([(r, r=="signal") for r in self.processes])
+        self.bins.append(name)
+        self.obs[name] = obs
+        self.keyline.extend([(name, process, process=="signal") for process in bkgRates.keys()]) # TODD check order, take ordered dict???
+        self.exp[name] = bkgRates
+
+        systDict = dict([(l[0],l) for l in self.systs])
+        for source, newUncerts in bkgUncertainties.iteritems():
+            for p in self.processes:
+                if p not in newUncerts:
+                    newUncerts[p] = 0
+            if source not in systDict:
+                systDict[source] = (source, False, "lnN", [], dict([(b,dict([(r,0) for r in self.processes])) for b in self.bins]))
+            systDict[source][4][name] = newUncerts
+        for source, line in systDict.iteritems():
+            for bin in self.bins:
+                if bin not in line[4]:
+                    systDict[source][4][bin] = dict([(r,0) for r in self.processes])
+        self.systs = sorted(systDict.values())
+
+
     def newSignal(self, infos):
         if sorted(infos.keys()) != sorted(self.bins): print "Error, not all bins replaced:", sorted(infos.keys()), sorted(self.bins)
         for b, (newRate, newUncert) in infos.iteritems():
@@ -280,6 +273,49 @@ class MyDatacard(Datacard):
             for process, rate in expDict.iteritems():
                 totRate += 0 if process == "signal" else rate
             self.obs[binName] = round(totRate)
+
+class Limit:
+    def __init__(self, datacardname):
+        self.datacardname = datacardname
+        self.limitfilename = datacardname+".limit"
+
+        self.obs = -1
+        self.exp = -1
+        self.expUp = -1
+        self.expDn = -1
+        self.exp2Up = -1
+        self.exp2Dn = -1
+        self.rMinNLL = -1
+        self.error = None
+
+        self.dc = MyDatacard(datacardname)
+
+    def calculate(self):
+        if not os.path.isfile(self.limitfilename) or os.path.getmtime(self.datacardname)>os.path.getmtime(self.limitfilename):
+            with open(self.limitfilename, "w+") as f:
+                bn = os.path.basename(self.datacardname)
+                out = subprocess.check_output(["combine", "-M", "Asymptotic", self.datacardname, "-n", bn], stderr=subprocess.STDOUT)
+                f.write(out)
+                outputFile = "higgsCombine{}.Asymptotic.mH120.root".format(bn)
+                if os.path.isfile(outputFile): os.remove(outputFile)
+        with open(self.limitfilename) as f:
+            out = f.read()
+
+    def getInfo(self):
+        if not os.path.isfile(self.limitfilename): self.calculate()
+        with open(self.limitfilename) as f:
+            out = f.read()
+        lines = out.split("\n")
+        for line in lines:
+            if line.startswith("NLL at global minimum of asimov:"): self.rMinNLL = float(re.match(".*\(r = (.*)\).*",line).group(1))
+            if line.startswith("Observed Limit: r < "): self.obs    = float(line.split("<")[1])
+            if line.startswith("Expected  2.5%: r < "): self.exp2Dn = float(line.split("<")[1])
+            if line.startswith("Expected 16.0%: r < "): self.expDn  = float(line.split("<")[1])
+            if line.startswith("Expected 50.0%: r < "): self.exp    = float(line.split("<")[1])
+            if line.startswith("Expected 84.0%: r < "): self.expUp  = float(line.split("<")[1])
+            if line.startswith("Expected 97.5%: r < "): self.exp2Up = float(line.split("<")[1])
+            if "ERROR" in line: self.error = line
+
 
 
 if False:
