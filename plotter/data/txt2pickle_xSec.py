c2cc1ab35aa33ce1486e2ba41f3d7625e254d7ea
Knut Kiesel <kiesel@cern.ch>
2017-02-11 18:41:00 +0100

    also extract weak signal cross section

--- plotter/data/txt2pickle_xSec.py
+++ plotter/data/txt2pickle_xSec.py
@@ -5,12 +5,7 @@ import argparse
 import re
 import pickle
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
-    args = parser.parse_args()
-
-
+def getXsecsStrong(filename):
     xSecs = {}
     with open( args.filename ) as f:
         for line in f.readlines():
@@ -18,7 +13,30 @@ if __name__ == "__main__":
             if match:
                 m_str, xSec_str, uncert_str = match.groups()
                 xSecs[ int(m_str) ] = ( float(xSec_str), float( uncert_str ) )
+    return xSecs
 
+def getXsecsWeak(filename):
+    xSecs = {}
+    with open( args.filename ) as f:
+        for line in f.readlines():
+            match = re.match("\s*(\d+)\s*([^\s]*)\s*([^\s]*)\s*", line )
+            if match:
+                m_str, xSec_str, uncert_str = match.groups()
+                xSecs[ int(m_str) ] = ( float(xSec_str)/1000, float( uncert_str ) / float( xSec_str ) )
+    return xSecs
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    filename = args.filename
+    if "Gluino" in filename or "Squark" in filename:
+        xSecs = getXsecsStrong(filename)
+    elif "N2C1" in filename or "C1C1" in filename:
+        xSecs = getXsecsWeak(filename)
 
     output = open( args.filename.replace("txt", "pkl" ), 'wb')
     pickle.dump(xSecs, output)
