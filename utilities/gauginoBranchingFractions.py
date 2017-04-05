217d2fab7a01fedcb9a9af13381c25cf9fcb1fe6
Knut Kiesel <kiesel@cern.ch>
2017-04-05 15:16:33 +0200

    improved plot style

--- utilities/gauginoBranchingFractions.py
+++ utilities/gauginoBranchingFractions.py
@@ -73,18 +73,18 @@ for f in bToZ, fcw2, fsw2:
     f.Draw("same")
 
 weinbergScale=0.8
-ROOT.gStyle.SetTextSize(.1)
-ROOT.gStyle.SetLabelSize(0.06, "xyz")
-ROOT.gStyle.SetTitleSize(0.06, "xyz")
+ROOT.gStyle.SetTextSize(.06)
+ROOT.gStyle.SetLabelSize(0.055, "xyz")
+ROOT.gStyle.SetTitleSize(0.055, "xyz")
 
 label = ROOT.TLatex()
-label.DrawLatexNDC( .2, .65, "#scale[%s]{cos^{2}(#theta_{W})}"%weinbergScale )
-label.DrawLatexNDC( .2, .33, "#scale[%s]{sin^{2}(#theta_{W})}"%weinbergScale )
+label.DrawLatexNDC( .17, .65, "#scale[%s]{cos^{2}(#theta_{W})}"%weinbergScale )
+label.DrawLatexNDC( .17, .33, "#scale[%s]{sin^{2}(#theta_{W})}"%weinbergScale )
 
-label.DrawLatexNDC( .5, .6, "#color[2]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + #gamma}" )
-label.DrawLatexNDC( .5, .3, "#color[4]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + Z}" )
+label.DrawLatexNDC( .6, .61, "#color[2]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + #gamma}" )
+label.DrawLatexNDC( .6, .32, "#color[4]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + Z}" )
 
-label.DrawLatexNDC( .33, .9, "Bino-like #tilde{#chi}^{0}_{1}" )
+label.DrawLatexNDC( .13, .92, "Bino-like #tilde{#chi}^{0}_{1}" )
 
 ROOT.gPad.SaveAs("binoBranching.pdf")
 
@@ -94,13 +94,13 @@ for f in wToZ, fcw2, fsw2:
     f.Draw("same")
 
 label = ROOT.TLatex()
-label.DrawLatexNDC( .2, .65, "#scale[%s]{cos^{2}(#theta_{W})}"%weinbergScale )
-label.DrawLatexNDC( .2, .33, "#scale[%s]{sin^{2}(#theta_{W})}"%weinbergScale )
+label.DrawLatexNDC( .17, .65, "#scale[%s]{cos^{2}(#theta_{W})}"%weinbergScale )
+label.DrawLatexNDC( .17, .33, "#scale[%s]{sin^{2}(#theta_{W})}"%weinbergScale )
 
-label.DrawLatexNDC( .5, .2, "#color[2]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + #gamma}" )
-label.DrawLatexNDC( .5, .7, "#color[4]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + Z}" )
+label.DrawLatexNDC( .6, .21, "#color[2]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + #gamma}" )
+label.DrawLatexNDC( .6, .73, "#color[4]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + Z}" )
 
-label.DrawLatexNDC( .33, .9, "Wino-like #tilde{#chi}^{0}_{1}" )
+label.DrawLatexNDC( .13, .92, "Wino-like #tilde{#chi}^{0}_{1}" )
 
 ROOT.gPad.SaveAs("winoBranching.pdf")
 
@@ -144,8 +144,8 @@ m1 = 500
 m2 = 1000
 tanb = 1.5
 n = -1
-# pars: m1, m2, tanb, eta
-par = ( 500, 1000, 1.5, -1 )
+par = ( m1, m2, tanb, n )
+#par = ( 500, 1000, 2.5, 1 )
 
 for f in hToG, hToZ, hToH:
     f.SetNpx(10000)
@@ -155,7 +155,7 @@ for f in hToG, hToZ, hToH:
     f.SetParameters( *par )
 hToG.SetLineColor( ROOT.kRed )
 hToZ.SetLineColor( ROOT.kBlue )
-hToH.SetLineColor( ROOT.kMagenta )
+hToH.SetLineColor( 28 )
 
 hToG.Draw()
 hToZ.Draw("same")
@@ -163,10 +163,11 @@ hToH.Draw("same")
 
 label.DrawLatexNDC( .5, .7, "#color[2]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + #gamma}" )
 label.DrawLatexNDC( .5, .55, "#color[4]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + Z}" )
-label.DrawLatexNDC( .5, .4, "#color[616]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + h}" )
+label.DrawLatexNDC( .5, .4, "#color[28]{#tilde{#chi}^{0}_{1}#rightarrow#tilde{G} + h}" )
 
-label.DrawLatexNDC( .01, .92, "#scale[0.7]{Higgsino-like #tilde{#chi}^{0}_{1}} #scale[0.3]{M_{1}=%sGeV, M_{2}=%sGeV, tan(#beta)=%s, #eta=%s}"%par )
+label.DrawLatexNDC( .13, .92, "Higgsino-like #tilde{#chi}^{0}_{1}")
+label.DrawLatexNDC( .55, .92, "#scale[.7]{#splitline{M_{1}=%sGeV, M_{2}=%sGeV}{tan(#beta)=%s, #eta=%s}}"%par )
 
 
-ROOT.gPad.SaveAs("higgsinoBranching_M1%s_M2%s_tanb%s_n%s.pdf"%(m1,m2,tanb,n))
+ROOT.gPad.SaveAs("higgsinoBranching_M1%s_M2%s_tanb%s_n%s.pdf"%par)
 
