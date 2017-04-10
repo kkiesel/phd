5d3e025433ea00ef1e2710216186fa65b9584992
Knut Kiesel <kiesel@cern.ch>
2017-04-10 12:17:41 +0200

    creating preliminary plots

--- plotter/compareSignalScans.py
+++ plotter/compareSignalScans.py
@@ -4,6 +4,7 @@ from include import *
 
 
 def compare(myFile, joFile, oldFile=""):
+    includeSUS15012 = False
     saveName = "compareLimits_"
     info = ""
 
@@ -41,16 +42,23 @@ def compare(myFile, joFile, oldFile=""):
     obsGr_jo.SetLineColor(ROOT.kBlue)
     expGr_jo.SetLineColor(ROOT.kBlue)
 
+    for gr in obsGr_knut, expGr_knut, obsGr_jo, expGr_jo:
+        gr.SetLineWidth(2)
+
     if oldFile:
         obsGr_old = aux.getFromFile(oldFile, "obs")
         expGr_old = aux.getFromFile(oldFile, "exp")
         obsGr_old.SetLineColor(ROOT.kMagenta)
         expGr_old.SetLineColor(ROOT.kMagenta)
         expGr_old.SetLineStyle(2)
+        for gr in obsGr_old, expGr_old:
+            gr.SetLineWidth(2)
 
     expGr_jo.SetLineStyle(2)
     expGr_knut.SetLineStyle(2)
 
+    h2.GetYaxis().SetLimits(0,3200)
+    if "T6" in myFile: h2.GetYaxis().SetLimits(0,2800)
     h2.Draw("colz")
     obsGr_knut.Draw("same")
     expGr_knut.Draw("same")
@@ -60,12 +68,14 @@ def compare(myFile, joFile, oldFile=""):
         expGr_old.Draw("same")
         obsGr_old.Draw("same")
 
-    if "gg" in myFile:
+    if "gg" in myFile and includeSUS15012:
         obsGr_old2 = ROOT.TGraph()
         expGr_old2 = ROOT.TGraph()
         obsGr_old2.SetLineColor(ROOT.kGreen+2)
         expGr_old2.SetLineColor(ROOT.kGreen+2)
         expGr_old2.SetLineStyle(2)
+        obsGr_old2.SetLineWidth(2)
+        expGr_old2.SetLineWidth(2)
         if "T5gg" in myFile:
             obsGr_old2.SetPoint(0, 1560, 100)
             obsGr_old2.SetPoint(1, 1610, 500)
@@ -84,42 +94,46 @@ def compare(myFile, joFile, oldFile=""):
         expGr_old2.Draw("same")
 
 
-    xmin = h2.GetXaxis().GetXmin()
+    xmin = h2.GetXaxis().GetBinLowEdge(h2.GetXaxis().GetFirst())
     xmax = h2.GetXaxis().GetXmax()
     line = ROOT.TLine(xmin,xmin,xmax,xmax)
-    line.SetLineColor(ROOT.kGray)
-    line.SetLineStyle(2)
+    line.SetLineColor(ROOT.kGray+2)
+    line.SetLineStyle(3)
     line.Draw()
 
     text = ROOT.TLatex()
-    text.DrawLatexNDC(0.15, .95, "#font[61]{CMS} #scale[0.76]{Private Work}" )
+    text.DrawLatexNDC(0.15, .95, "#font[61]{CMS} #scale[0.76]{Preliminary}" )
     text.DrawLatexNDC(0.2, .87, info )
-    text.DrawLatexNDC( .59, .95, "%.2f fb^{-1} (%s TeV)"%(aux.intLumi/1000., 13) )
+    text.DrawLatexNDC( .59, .95, "%.1f fb^{-1} (%s TeV)"%(aux.intLumi/1000., 13) )
 
     obsGr = obsGr_knut.Clone(aux.randomName())
     expGr = expGr_knut.Clone(aux.randomName())
     obsGr.SetLineColor(ROOT.kBlack)
     expGr.SetLineColor(ROOT.kBlack)
 
-    leg = ROOT.TLegend(.2,.5,.6,.85)
+    legBlack = ROOT.TLegend(.67,.74,.97,.84)
+    legBlack.SetFillStyle(0)
+    legBlack.AddEntry(obsGr, "Observed")
+    legBlack.AddEntry(expGr, "Expected")
+    legBlack.Draw()
+
+    leg = ROOT.TLegend(.16,.74,.65,.84)
+    if oldFile: leg = ROOT.TLegend(.16,.65,.69,.84)
     leg.SetFillStyle(0)
-    leg.AddEntry(obsGr, "Observed")
-    leg.AddEntry(expGr, "Expected")
     leg.AddEntry(obsGr_knut, "SUS-16-047: high EMH_{T}")
     leg.AddEntry(obsGr_jo, "SUS-16-046: high S_{T}^{#gamma}")
-    if oldFile: leg.AddEntry(obsGr_old, "SUS-16-023: high S_{T}^{#gamma} 2.3fb^{-1}")
-    if "gg" in myFile:
+    if oldFile: leg.AddEntry(obsGr_old, "SUS-16-023: high S_{T}^{#gamma} 2.3 fb^{-1}")
+    if "gg" in myFile and includeSUS15012:
         leg.AddEntry(obsGr_old2, "SUS-15-012: 2#gamma 2.3fb^{-1}", "l")
-
     leg.Draw()
 
-    aux.save(saveName, log=False)
+    aux.save("forChristian_"+saveName, log=False)
 
 
 
 
 if __name__ == "__main__":
-    compare("limitCalculations/T6gg_v9/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T6gg.root")
-    compare("limitCalculations/T6Wg_v9/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T6wg.root")
-    compare("limitCalculations/T5gg_v9/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T5gg.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-PAS-SUS-16-023-T5gg.root")
-    compare("limitCalculations/T5Wg_v9/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T5wg.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-PAS-SUS-16-023-T5wg.root")
+    compare("limitCalculations/T6gg_v11/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T6gg.root")
+    compare("limitCalculations/T6Wg_v11/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T6wg.root")
+    compare("limitCalculations/T5gg_v11/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T5gg.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-PAS-SUS-16-023-T5gg.root")
+    compare("limitCalculations/T5Wg_v11/saved_graphs1d_limit.root", "/home/home4/institut_1b/kiesel/other_photon_limits/CMS-SUS-16-046-T5wg.root")
